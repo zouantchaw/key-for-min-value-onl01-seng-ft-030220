eb3b58f098ae2ef618867f360523908ce2bd2ceb
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-03-15 13:28:33 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   lowest_key = nil
   lowest_value = nil
-  hash.each do |k, v|
+  name_hash.each do |k, v|
     if lowest_value == nil || v < lowest_value
       lowest_value = v
       lowest_key = k
